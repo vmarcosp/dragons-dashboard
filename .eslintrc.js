03a84107b0d844a704932ee4e4059fbe56b6ffb7
vmarcosp <marcosvcontato.mv@gmail.com>
2017-08-01 20:01:09 -0300

    Created structure of project

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  extends: 'standard',
+  plugins: [
+    'html'
+  ],
+  'rules': {
+    "space-before-function-paren": ["error", "never"],
+    "indent": 0,
+    "semi": [2, "always"],
+    'arrow-parens': 0,
+    'generator-star-spacing': 0,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+}
