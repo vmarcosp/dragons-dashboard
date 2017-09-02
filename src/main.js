dc4337be8f475e1722f7d338ec33709b37663dd9
vmarcosp <contato@vmarcosp.com.br>
2017-09-02 16:11:17 -0300

    Configured vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import store from './vuex/store';
+
+// CSS Imports
 import 'font-awesome/css/font-awesome.css';
 
 Vue.config.productionTip = false;
@@ -8,7 +11,10 @@ Vue.config.productionTip = false;
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   template: '<App/>',
-  components: { App }
+  components: {
+    App
+  }
 });
