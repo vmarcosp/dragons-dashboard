d1cfa68df9b33b56cb71ae53add6743abde851d7
Marcos Vinicius <contato@vmarcosp.com.br>
2017-09-04 10:40:19 -0300

    Updated description of project

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <page-title value="The project"></page-title>
-    <p>This project have intention of show how consume a API rest using VueJS. The layout was developed using the components of <strong>UI Kit HBSIS</strong>. The UI Kit HBSIS is a project developed by HBSIS's front-end engineers and designers, than has as objective define a pattern for user interface.</p>
+    <p>This project has intention of show how consume a API rest using VueJS. The layout was developed using the components of <strong>UI Kit HBSIS</strong>. The UI Kit HBSIS is a project developed by HBSIS's front-end engineers and designers, that has as objective define a pattern for user interface.</p>
   </div>
 </template>
 
